5150c19a114eddd236d983c4e9b2cd92eefe08aa
Ravi <0001rkm@gmail.com>
2018-12-29 01:18:02 +0530

    header added

---
+++ letsmooove/src/components/header/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const Header = () => {
+  return (
+    <div>
+      HEAD
+      <div>aboutUs</div>
+      <div>HowItWrks</div>
+      <div>contactUs</div>
+    </div>
+  );
+};
+export default Header;
